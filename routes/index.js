170718351901bea87426159419cf596e00ae3594
Takayuki Higuchi <higutaka7183@gmail.com>
2024-10-18 15:46:11 +0900

    Vue導入でテスト対象の関数名が変わっていたので、以前のものに修正

--- routes/index.js
+++ routes/index.js
@@ -14,14 +14,25 @@ router.get('/', (req, res, next) => {
 
 // 足し算結果を取得するAPI
 router.get('/api/sum', (req, res) => {
-  const sum = 1 + 7;
+  const sum = add(1,7);
   res.json({ sum: sum });
 });
 
 // 日付を取得するAPI
 router.get('/api/date', (req, res) => {
-  const today = new Date().toLocaleDateString();
+  const today = getCurrentDate();
   res.json({ date: today });
 });
+
+//足し算を実行する関数
+export function add(a, b) {
+  return a + b;
+}
+
+//今日の日付を文字列で取得
+export function getCurrentDate() {
+  const date = new Date();
+  return date.getFullYear() + '/' + (date.getMonth()+1) + '/' + date.getDate();
+}
 
 export default router;
